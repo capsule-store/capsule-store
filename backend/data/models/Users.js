b01ba197897aa26e0247dea0f71ab072ed9d3e4e
Stella Kim <testcodesk@gmail.com>
2019-10-15 20:09:26 -0400

    user model added

--- backend/data/models/Users.js
+++ backend/data/models/Users.js
@@ -2,24 +2,24 @@ const conn = require("./conn");
 const { Sequelize } = conn;
 const { STRING, UUID, UUIDV4 } = Sequelize;
 
-const user = conn.define('user', {
+const user = conn.define("user", {
   id: {
     type: UUID,
     primaryKey: true,
-    defaultValue: UUIDV4,
+    defaultValue: UUIDV4
   },
   firstName: {
     type: STRING,
     allowNull: false,
     validate: {
-      len: [1,255]
+      len: [1, 255]
     }
   },
   lastName: {
     type: STRING,
     allowNull: false,
     validate: {
-      len: [1,255]
+      len: [1, 255]
     }
   },
   email: {
@@ -27,16 +27,16 @@ const user = conn.define('user', {
     allowNull: false,
     unique: true,
     validate: {
-      len: [1,255]
+      len: [1, 255]
     }
   },
   password: {
     type: STRING,
-    allowNull:false,
+    allowNull: false,
     validate: {
-      len: [6,255]
+      len: [6, 255]
     }
   }
-})
+});
 
 module.exports = User;
