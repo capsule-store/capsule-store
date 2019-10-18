1d246c0b2d3df001ee27f4a2dbd05addb942a4ed
Jesen Tanadi <mail@jesentanadi.com>
2019-10-18 08:16:10 -0400

    set up basic redux store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import combinedReducer from './reducers';
+
+const store = createStore(combinedReducer, applyMiddleware(thunk));
+
+export default store;
