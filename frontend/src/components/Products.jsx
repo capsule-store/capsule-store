a99d60396cf17b78f48498c929c084b6f1c88d3a
Jesen Tanadi <mail@jesentanadi.com>
2019-10-20 14:15:41 -0400

    add Products page

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Products = () => (
+const Products = ({ products }) => (
   <div>
-    <h1>Products</h1>
+    <h2>Products</h2>
+    <ul>
+      {// In the end we should use <ProductCard/> components
+      products.map((product) => (
+        <li key={product.id}>
+          <div>{product.name}</div>
+          <div>{product.price}</div>
+        </li>
+      ))
+}
+    </ul>
   </div>
 );
 
-export default Products;
+const mapStateToProps = ({ products }) => ({ products });
+
+export default connect(mapStateToProps)(Products);
