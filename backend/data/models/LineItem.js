3b25eb879f22b2d91fa6cde198393299de4fda76
Jesen Tanadi <mail@jesentanadi.com>
2019-10-20 16:50:35 -0400

    linted

--- backend/data/models/LineItem.js
+++ backend/data/models/LineItem.js
@@ -1,23 +1,23 @@
+const Sequelize = require('sequelize');
 const conn = require('../connection');
-const Sequelize = require ('sequelize');
-const {UUID, UUIDV4, INTEGER} = Sequelize;
+
+const { UUID, UUIDV4, INTEGER } = Sequelize;
 
 const LineItem = conn.define('lineItem', {
-  id:{
+  id: {
     primaryKey: true,
     type: UUID,
-    defaultValue: UUIDV4
+    defaultValue: UUIDV4,
   },
-
-  quantity:{
+  quantity: {
     type: INTEGER,
     allowNull: false,
     unique: false,
     validate: {
       notEmpty: true,
-      min: 0
-    }
-  }
+      min: 0,
+    },
+  },
 });
 
 module.exports = LineItem;
