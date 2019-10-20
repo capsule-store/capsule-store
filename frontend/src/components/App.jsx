34ec578e9d6338242e9567fefcf0e9dfdd07d732
Jesen Tanadi <mail@jesentanadi.com>
2019-10-20 14:13:27 -0400

    fetch data

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,24 +1,28 @@
+// Package imports
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
   HashRouter, Route, Switch, Redirect,
 } from 'react-router-dom';
 
+// Local imports
 import { actions } from '../store';
 import { testLineItems } from '../testData';
 
 // Components
-import Login from './Login';
 import Home from './Home';
-import Register from './Register';
 import Nav from './Nav';
+import Products from './Products';
 import Cart from './Cart';
+import Login from './Login';
+import Register from './Register';
 
 /* App */
 class _App extends Component {
   componentDidMount() {
-    const { attemptSessionLogin } = this.props;
+    const { attemptSessionLogin, loadStoreData } = this.props;
     attemptSessionLogin().catch((ex) => console.log(ex));
+    loadStoreData();
   }
 
   render() {
@@ -30,6 +34,7 @@ class _App extends Component {
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/products" component={Products} />
             <Route
               path="/cart"
               render={() => <Cart lineItems={testLineItems} />}
@@ -49,6 +54,11 @@ const mapStateToProps = ({ auth }) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   attemptSessionLogin: () => dispatch(actions.attemptSessionLogin()),
+  loadStoreData: () => {
+    // fetch all necessary store data (not related to user)
+    dispatch(actions.fetchProducts());
+    dispatch(actions.fetchCategories());
+  },
 });
 
 const App = connect(
