df3ec99e37932af2db050aa70451b73420d2e8c0
Jesen Tanadi <mail@jesentanadi.com>
2019-10-22 07:15:39 -0400

    add buttons to each line item (+/-, delete)

--- frontend/src/components/LineItem.jsx
+++ frontend/src/components/LineItem.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { actions } from '../store';
 
-const LineItem = ({ item }) => (
+const LineItem = ({ item, updateQuantity, removeLineItem }) => (
   <li className="lineItem">
     <div className="itemName">{item.name}</div>
+    <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>
+      -
+    </button>
     <div className="itemQuantity">{item.quantity}</div>
+    <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>
+      +
+    </button>
     <div className="itemPrice">{item.price}</div>
     <div className="itemSubTotal">{item.quantity * item.price}</div>
+    <button onClick={() => removeLineItem(item.id)}>Remove</button>
   </li>
 );
 
-export default LineItem;
+const mapDispatchToProps = ({ dispatch }) => ({
+  updateQuantity: (id, quantity) => {
+    dispatch(actions.updateLineItem(id, quantity));
+  },
+  removeLineItem: (id) => {
+    dispatch(actions.removeLineItem(id));
+  },
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(LineItem);
