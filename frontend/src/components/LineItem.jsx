9baf0e5f2a995700e658c0c1d0cc311fb209e4cd
Jesen Tanadi <mail@jesentanadi.com>
2019-10-22 08:08:21 -0400

    add button type

--- frontend/src/components/LineItem.jsx
+++ frontend/src/components/LineItem.jsx
@@ -4,16 +4,29 @@ import { actions } from '../store';
 const LineItem = ({ item, updateQuantity, removeLineItem }) => (
   <li className="lineItem">
     <div className="itemName">{item.name}</div>
-    <button onClick={() => updateQuantity(item.id, item.quantity - 1)}>
+
+    <button
+      type="button"
+      onClick={() => updateQuantity(item.id, item.quantity - 1)}
+    >
       -
     </button>
+
     <div className="itemQuantity">{item.quantity}</div>
-    <button onClick={() => updateQuantity(item.id, item.quantity + 1)}>
+
+    <button
+      type="button"
+      onClick={() => updateQuantity(item.id, item.quantity + 1)}
+    >
       +
     </button>
+
     <div className="itemPrice">{item.price}</div>
     <div className="itemSubTotal">{item.quantity * item.price}</div>
-    <button onClick={() => removeLineItem(item.id)}>Remove</button>
+
+    <button type="button" onClick={() => removeLineItem(item.id)}>
+      Remove
+    </button>
   </li>
 );
 
