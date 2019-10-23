f88eed40781f8f1a2e18da6155212ed4236fbfae
Jesen Tanadi <mail@jesentanadi.com>
2019-10-23 19:18:05 -0400

    flesh out product detail page

--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions } from '../store';
 
-class ProductDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: {},
-      quantity: 1,
-    };
-  }
+import ProductPurchase from './ProductPurchase';
 
-  render() {
-    return null;
-  }
-}
+const ProductDetail = ({ products }) => {
+  const product = products.find((prod) => prod.id === params.id);
+  return (
+    <div className="product-detail">
+      <h2 className="name">{product.name}</h2>
+      <h3 className="brand">{product.brand}</h3>
+      <ProductPurchase id={product.id} />
+      <img src={product.image} />
+    </div>
+  );
+};
 
-export default ProductDetail;
+const mapStateToProps = ({ products }) => ({
+  products,
+});
+
+export default connect(mapStateToProps)(ProductDetail);
