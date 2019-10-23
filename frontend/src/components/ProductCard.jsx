345555536e14d352eca1d362cb3d210d18da46c5
Jesen Tanadi <mail@jesentanadi.com>
2019-10-22 21:04:06 -0400

    first stab at card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const ProductCard = () => (
-  <div>
-    <h1>Product Card</h1>
+const ProductCard = ({ product }) => (
+  <div className="product-card">
+    <div className="info">
+      <h3 className="name">{product.name}</h3>
+      <p className="category">{product.category}</p>
+      <p className="brand">{product.brand}</p>
+      <p className="price">{product.price}</p>
+    </div>
+    <img
+      className="product-background"
+      src={product.image}
+      alt={`image of ${product.name}`}
+    />
   </div>
 );
 
