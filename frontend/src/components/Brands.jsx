187628461a18175226cd9b136d7b0524e1176ea6
Effy <yifei0730@gmail.com>
2019-10-26 14:03:42 -0400

    Add brands section style (#61)
    * updated nav style
    
    * add brands style
    
    * add brand links

--- frontend/src/components/Brands.jsx
+++ frontend/src/components/Brands.jsx
@@ -1,28 +1,66 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Div = styled.div`
-width: 100px;
+const Section = styled.div`
+width: 100%;
 `;
 
-// console.log('STYLE LI:', styledLi);
-const Brands = ({ brands }) => (
-  <Div>
-    <div>
-      <ul>
-        {brands.map((brand) => (
-          <li key={brand.id}>
-          <Link to={`/brands/${brand.id}`}>
-            <img src={`${brand.image}`} alt={brand.name} />
-          </Link>
+const Title = styled.div`
+  width:100%;
+  display:flex;
+`;
 
-        </li>
-        ))}
-      </ul>
-    </div>
-  </Div>
+const Hr = styled.div`
+display: block;
+height: 1px;
+flex-grow: 2;
+background-color: #000;
+margin: 12px 2rem;
+`;
+
+const BrandList = styled.div`
+width: 128px;
+transform: rotate(-90deg) translateY(-240px);
+transform-origin: right top;
+overflow-y: auto;
+margin:3rem 0;
+overflow-x: hidden;
+`;
+
+const Brand = styled.a`
+display:inline-block;
+width: 240px; 
+height: 128px;
+filter: grayscale(100%);
+margin: 6rem 0;
+transform: rotate(90deg)  translateY(112px);
+transform-origin: right top;
+`;
+
+const Logo = styled.img`
+width: 100%;
+object-fit: cover;
+`;
+
+const Brands = ({ brands }) => (
+  <Section>
+    <Title>
+      <Hr />
+      <h6>All Brands</h6>
+      <Hr />
+    </Title>
+    <BrandList>
+      {brands.map((brand) => (
+        <Link to={`/brands/${brand.id}`}>
+          <Brand key={brand.id}>
+            <Logo src={brand.image} />
+          </Brand>
+        </Link>
+      ))}
+    </BrandList>
+  </Section>
 );
 
 const mapStateToProps = ({ brands }) => ({ brands });
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import styled, { ThemeProvider } from 'styled-components';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import styled, { ThemeProvider } from "styled-components";
 
 /* Navigator */
 const StyledNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+const Left = styled.div`
+  flex-grow: auto;
+  display: flex;
+  align-self: flex-start;
+`;
+
+const Right = styled.div`
+  flex-grow: auto;
+  display: flex;
+  align-self: flex-end;
+`;
+
+const Home = styled.h3`
+  display: inline-block;
+  height: 1.5rem;
+  margin: 0;
+  flex-grow: 1;
+  align-self: center;
+  color: #000;
+`;
+
+const Icon = styled.div`
+  display: block;
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+const User = styled.div`
+  display: block;
+  margin: 0 2rem;
+  color: #000;
+`;
+
+const Promo = styled.div`
+  font-size: 0.8rem;
+  font-weight: 400;
+  height: 1.5rem;
+  line-height: 1.5rem;
+  display: block;
+  margin: 0 2rem;
+`;
+
+const Product = styled(Icon)`
+  background-image: url(../assets/images/icon/menu.svg);
+`;
+const Search = styled(Icon)`
+  background-image: url(../assets/images/icon/search.svg);
+  margin: 0 2rem;
+`;
+const Cart = styled(Icon)`
+  background-image: url(../assets/images/icon/cart.svg);
   display: flex;
   justify-content: space-between;
   padding: 2rem;
@@ -81,8 +137,8 @@ const _Nav = ({ loggedIn }) => (
     <Right>
       {/* <Promo>FREE SHIPPING –– on all orders over $50*</Promo> */}
       <User>
-        <NavLink to={loggedIn ? '/logout' : '/login'}>
-          {loggedIn ? 'Sign out' : 'Sign in'}
+        <NavLink to={loggedIn ? "/logout" : "/login"}>
+          {loggedIn ? "Sign out" : "Sign in"}
         </NavLink>
       </User>
       <NavLink to="/cart">
@@ -93,7 +149,7 @@ const _Nav = ({ loggedIn }) => (
 );
 
 const mapStateToProps = ({ auth }) => ({
-  loggedIn: !!auth.id,
+  loggedIn: !!auth.id
 });
 
 const Nav = connect(mapStateToProps)(_Nav);
