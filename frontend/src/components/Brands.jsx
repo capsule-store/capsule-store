57f43fc0c489879b10b4efa7e2c7c26f87cd824d
Jesen Tanadi <mail@jesentanadi.com>
2019-10-27 14:56:49 -0400

    fix nesting & key errors

--- frontend/src/components/Brands.jsx
+++ frontend/src/components/Brands.jsx
@@ -4,44 +4,44 @@ import styled, { ThemeProvider } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Section = styled.div`
-width: 100%;
+  width: 100%;
 `;
 
 const Title = styled.div`
-  width:100%;
-  display:flex;
+  width: 100%;
+  display: flex;
 `;
 
 const Hr = styled.div`
-display: block;
-height: 1px;
-flex-grow: 2;
-background-color: #000;
-margin: 12px 2rem;
+  display: block;
+  height: 1px;
+  flex-grow: 2;
+  background-color: #000;
+  margin: 12px 2rem;
 `;
 
 const BrandList = styled.div`
-width: 128px;
-transform: rotate(-90deg) translateY(-240px);
-transform-origin: right top;
-overflow-y: auto;
-margin:3rem 0;
-overflow-x: hidden;
+  width: 128px;
+  transform: rotate(-90deg) translateY(-240px);
+  transform-origin: right top;
+  overflow-y: auto;
+  margin: 3rem 0;
+  overflow-x: hidden;
 `;
 
-const Brand = styled.a`
-display:inline-block;
-width: 240px; 
-height: 128px;
-filter: grayscale(100%);
-margin: 6rem 0;
-transform: rotate(90deg)  translateY(112px);
-transform-origin: right top;
+const Brand = styled.div`
+  display: inline-block;
+  width: 240px;
+  height: 128px;
+  filter: grayscale(100%);
+  margin: 6rem 0;
+  transform: rotate(90deg) translateY(112px);
+  transform-origin: right top;
 `;
 
 const Logo = styled.img`
-width: 100%;
-object-fit: cover;
+  width: 100%;
+  object-fit: cover;
 `;
 
 const Brands = ({ brands }) => (
@@ -53,11 +53,11 @@ const Brands = ({ brands }) => (
     </Title>
     <BrandList>
       {brands.map((brand) => (
-        <Link to={`/brands/${brand.id}`}>
-          <Brand key={brand.id}>
+        <Brand key={brand.id}>
+          <Link to={`/brands/${brand.id}`}>
             <Logo src={brand.image} />
-          </Brand>
-        </Link>
+          </Link>
+        </Brand>
       ))}
     </BrandList>
   </Section>
