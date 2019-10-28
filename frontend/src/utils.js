a9f3b008372dced4b9e141dfb68c6ea05794f3a7
Jesen Tanadi <mail@jesentanadi.com>
2019-10-27 22:00:26 -0400

    fix error

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,9 +1,10 @@
 const padPrice = (price) => {
-  if (price.toString().includes('.')) {
-    const cents = price.split('.')[1].padEnd(2, '0');
-    return `${price.split('.')[0]}.${cents}`;
+  const priceStr = price.toString();
+  if (priceStr.includes('.')) {
+    const cents = priceStr.split('.')[1].padEnd(2, '0');
+    return `${priceStr.split('.')[0]}.${cents}`;
   }
-  return `${price}.00`;
+  return `${priceStr}.00`;
 };
 
 export { padPrice };
