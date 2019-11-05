eb147dc5b661230112f04ee791527f1b47203553
Stella Kim <testcodesk@gmail.com>
2019-11-05 00:31:21 -0500

    merge

--- backend/routes/auth-google.js
+++ backend/routes/auth-google.js
@@ -1,23 +1,77 @@
 const express = require('express');
-const passport = require('passport');
+const { google } = require('googleapis');
+const dotenv = require('dotenv').config();
+const axios = require('axios');
+const jwt = require('jwt-simple');
+// const plus = google.plus('v1');
 const router = express.Router();
-require('../config/google-passport').passport
+const { User } = require('../data').models;
 
-router.get(
-  '/',
-  passport.authenticate('google', {
-    scope: ['profile'],
-  }),
+router.use(express.json());
+
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL,
+} = process.env;
+
+const oauth2Client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL,
 );
+
+google.options({ auth: oauth2Client });
+
+const scopes = ['profile', 'email'];
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+  scope: scopes,
+});
+
+router.get('/', (req, res) => {
+  res.redirect(url);
+});
 
 // call back route for google oauth2
-router.get('/callback', 
-  passport.authenticate('google', { 
-    failureRedirect: '/login' 
-  }), (req, res) => {
-    console.log(req)
-    res.redirect(`/`)
-  }
-);
+router.get('/callback', async (req, res) => {
+  const { code } = req.query;
+
+  await oauth2Client.getToken(code, async (err, tokens) => {
+    try {
+      oauth2Client.setCredentials(tokens);
+      const token = tokens.id_token;
+      const userData = (await axios.get(
+        `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`,
+      )).data;
+
+      let user = await User.findOne({
+        where: {
+          googleId: userData.sub,
+          email: userData.email,
+        },
+      });
+
+      if (!user) {
+        user = (await User.create({
+          firstName: userData.given_name,
+          lastName: userData.family_name,
+          email: userData.email,
+          password: userData.sub,
+          googleId: userData.sub,
+        })).dataValues;
+        // console.log("created user is", user);
+      }
+
+      const authToken = jwt.encode({ id: userData.sub }, process.env.SECRET);
+      req.session.user = authToken;
+      res.redirect(`/?token=${authToken}`);
+    } catch (ex) {
+      // throw ex
+      console.log('err--->', ex);
+    }
+  });
+});
 
 module.exports = router;
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,10 +2,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-  HashRouter, Route, Switch, Redirect,
+ HashRouter, Route, Switch, Redirect 
 } from 'react-router-dom';
 
 // Local imports
+import styled from 'styled-components';
 import { actions } from '../store';
 
 // Components
@@ -19,21 +20,26 @@ import Logout from './Logout';
 import Register from './Register';
 import Brands from './Brands';
 import BrandDetail from './BrandDetail';
-import Categories from './Categories';
+import AdminProducts from './AdminProducts';
+import CreateProduct from './CreateProduct';
+
+const Main = styled.div`
+  width: 100hv;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 3rem auto 480px;
+  overflow-x: hidden;
+  grid-column-gap: 40px;
+  grid-row-gap: 40px;
+  margin: 40px;
+`;
 
 class _App extends Component {
-  componentDidUpdate() {
-    const { loadCart, loggedIn } = this.props;
-    loadCart(loggedIn);
-  }
-
-  componentDidMount() {
+  constructor(props) {
+    super(props);
     const {
-      loggedIn,
-      attemptSessionLogin,
-      loadStoreData,
-      loadCart,
-    } = this.props;
+ loggedIn, attemptSessionLogin, loadStoreData, loadCart 
+} = props;
 
     attemptSessionLogin().catch((ex) => console.log(ex));
     loadStoreData();
@@ -41,30 +47,50 @@ class _App extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <HashRouter>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/products" component={Products} />
-            <Route path="/products/:id" component={ProductDetail} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/signup" component={Register} />
-            <Route path="/brands/:id" component={BrandDetail} />
-          </Switch>
-          <Brands />
-        </HashRouter>
-      </div>
-    );
+    const {
+ products, brands, isAdmin, location 
+} = this.props;
+
+    let { loggedIn } = this.props;
+
+    if (!products.length || !brands.length) return null;
+
+    if (location.href.includes('?token=')) {
+      const token = location.href.split('?token=')[1];
+      loggedIn = token ? true : loggedIn;
+
+      return (
+        <Main>
+          <HashRouter>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/products" component={Products} />
+              <Route path="/products/:id" component={ProductDetail} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/login" component={Login} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/signup" component={Register} />
+              <Route path="/brands/:id" component={BrandDetail} />
+              <Route exact path="/admin">
+                {!isAdmin ? <Redirect to="/" /> : <AdminProducts />}
+              </Route>
+              <Route path="/admin/create-product" component={CreateProduct} />
+            </Switch>
+            <Brands />
+          </HashRouter>
+        </Main>
+      );
+    }
   }
 }
 
-const mapStateToProps = ({ auth }) => ({
+const mapStateToProps = ({ auth, products, brands }) => ({
   loggedIn: !!auth.id,
+  isAdmin: auth.isAdmin,
   auth,
+  products,
+  brands,
 });
 
 const mapDispatchToProps = (dispatch) => ({
