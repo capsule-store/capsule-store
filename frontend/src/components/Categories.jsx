e6e687a53f4ec761b368593ac6b7cb697ce800f5
Effy Zhang <effy@effy-0qlvch.local>
2019-11-04 19:49:32 -0500

    fixed hover flash

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -5,26 +5,27 @@ import { Link } from 'react-router-dom';
 
 
 const CategoryList = styled.div`
-grid-area: 2/1/3/2;
-min-width: 280px;
-display: flex;
-flex-direction: column;
+  grid-area: 2/1/3/2;
+  min-width: 280px;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Category = styled.p`
-&:hover{
-  font-size: 1.5rem;
-  font-weight: 600;
-  margin:  2rem 0;
-}
-${(props) => (props.active ? `
+const Category = styled.div`
+  margin: 0;
+  padding: 1rem 0;
+
+  &:hover{
     font-size: 1.5rem;
     font-weight: 600;
-    margin:  2rem 0;
-
-
-` : `
-`)}
+    margin-bottom: -10px;
+  }
+  ${(props) => (props.active ? `
+      font-size: 1.5rem;
+      font-weight: 600;
+      margin-bottom: -10px;
+  ` : `
+  `)}
 `;
 
 class Categories extends Component {
@@ -51,6 +52,7 @@ class Categories extends Component {
           <Link to={`${category.name}`.toLowerCase()} key={category.id}>
             <Category
               onClick={(ev) => updateCategory(category.name)}
+              active={category.name.toLowerCase() === current.toLowerCase()}
             >
               {category.name}
             </Category>
